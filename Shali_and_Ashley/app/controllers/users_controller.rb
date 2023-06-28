e303eb902ddb9bd391b800df3ea9251941e58df1
Ashley Yan <Asyan77@gmail.com>
2023-06-28 16:37:30 -0700

    work on part 3 of project

---
+++ Shali_and_Ashley/app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+    def index
+        render json: params
+    end
+
+    def create
+        render json: params
+    end
+
+    def show
+        render json: params
+    end
+    
+    def delete
+        render json: params
+    end
+
+    def update
+        render json: params
+    end
+end
